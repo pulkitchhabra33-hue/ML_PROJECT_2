054fdb9e45c328ca9ca538fec618f37482ef8062
pulkitchhabra33-hue <pulkitchhabra33@gmail.com>
2026-01-29 15:17:12 +0530

    template.py adding

--- template.py
+++ template.py
@@ -19,7 +19,7 @@ list_of_files= [
     f"src/{project_name}/exception.py",
     f"src/{project_name}/logger.py",
     f"src/{project_name}/utils.py",
-    "main..py",
+    "main.py",
     "app.py",
     "Dockerfile",
     "requirements.txt",
